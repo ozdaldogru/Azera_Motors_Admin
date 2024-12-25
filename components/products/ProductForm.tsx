8222a2882b780599255331909a503698ca5daaf7
Ozdal Dogru <ozdaldogru@gmail.com>
2024-12-25 01:41:18 -0500

    Add custom words to cSpell and refactor ProductForm to use Textarea component

--- components/products/ProductForm.tsx
+++ components/products/ProductForm.tsx
@@ -15,6 +15,7 @@ import toast from "react-hot-toast";
 import Delete from "../custom ui/Delete";
 import MultiSelectFeature from "../custom ui/MultiSelectFeature";
 import Loader from "../custom ui/Loader";
+import { Textarea } from "../ui/textarea";
 import {CarMakes} from "../Shared/CarMakes";
 import {Categories} from "../Shared/Category";
 import {Conditions} from "../Shared/conditions";
@@ -640,7 +641,7 @@ const ProductForm: React.FC<ProductFormProps> = ({ initialData }) => {
                       <FormItem>
                         <FormLabel>Description</FormLabel>
                         <FormControl>
-                          <textarea
+                          <Textarea
                             className="flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
           
                             aria-label="enter detailed description"
