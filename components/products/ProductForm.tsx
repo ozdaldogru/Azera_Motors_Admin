5df11989bcad7344cfae45d1fc9b03f2b1336142
Ozdal Dogru <ozdaldogru@gmail.com>
2025-01-01 13:30:02 -0500

    Replace textarea with JoditEditor for description input in ProductForm

--- components/products/ProductForm.tsx
+++ components/products/ProductForm.tsx
@@ -23,7 +23,7 @@ import {driveTypes} from "../Shared/drivetype";
 import {fuelTypes} from "../Shared/fueltype";
 import {Statuses} from "../Shared/statuses";
 import {transmissions} from "../Shared/transmission";
-//import JoditEditor from 'jodit-react';
+import JoditEditor from 'jodit-react';
 
 
 const formSchema = z.object({
@@ -683,11 +683,8 @@ const ProductForm: React.FC<ProductFormProps> = ({ initialData }) => {
                       <FormItem>
                         <FormLabel>Description</FormLabel>
                         <FormControl>
-                          <textarea
-                            className="flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
-          
-                            aria-label="enter detailed description"
-                            placeholder="Description"
+                        <JoditEditor
+                         
                             {...field}
                             
                           />
